16b855ca5e24870e69698cacfaae2307574d94a3
Jet Eliens <jetelie@live.nl>
2021-01-08 16:23:04 +0100

    updated

--- code/visualisation/visualise.py
+++ code/visualisation/visualise.py
@@ -1,23 +1,29 @@
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-def visualise(map):
+def visualise(self, kaart):
+    x = []
+    y = []
 
-    positions = [station for station in map.stations.values()]
-    name = [station.name for station in positions]
-    color = [station.get_value().colour.get_web() if station is not None else "grey"
-              for station in positions]
+    for station in kaart.stations:
+        x.append(station.x_position)
+        y.append(station.y_position)
 
+    print(x)
+    print("hello")
+    print(y)
 
-    for station in test_map.stations:
-        station.add_directions(connections_file)
+    plt.figure(figsize=(9, 3))
 
-        x = test_map.stations.x_position
-        y = test_map.stations.y_position
+    plt.scatter(x, y)
+    # plt.scatter(x, y, s=80, c=z, marker=("."))
+    plt.set_title("Stations")
 
-    fig, axs = plt.subplots(2, 3, sharex=True, sharey=True)
-
-    axs[1, 0].scatter(x, y, s=80, c=z, marker=("."))
-    axs[1, 0].set_title("Stations")
-
-    plt.tight_layout()
+    # plt.tight_layout()
     plt.show()
+
+    # positions = [station for station in map.stations.values()]
+    # name = [station.name for station in positions]
+    # color = [station.get_value().colour.get_web() if station is not None else "grey"
+    #           for station in positions]

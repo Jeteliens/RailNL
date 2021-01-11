b31ef22b955097e554aa8d2432c488d7cd935cad
Jet Eliens <jetelie@live.nl>
2021-01-11 12:36:09 +0100

    updated

--- test/visualise.py
+++ test/visualise.py
@@ -4,19 +4,31 @@ import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 
 def visualise():
-    x = [3, 5, 9]
-    y = [2, 6, 8]
+    x = [3, 5, 9, 12]
+    y = [2, 6, 8, 13]
+    n = ['Amsterdam', 'Alkmaar', 'Haarlem', 'Delft']
+
+    # BBox = ((df.longitude.min(3.2932),   df.longitude.max(6.2869),      
+        #  df.latitude.min(52.9701), df.latitude.max(51.6998))
+
 
     # for station in kaart.stations:
     #     x.append(station.x_position)
     #     y.append(station.y_position)
 
-    plt.figure(figsize=(9, 3))
+    # img = plt.imread("kaart1.png")
+    plt.figure()
+    # plt.imshow(img)
+    plt.plot(x, y, c='g', marker=("."))
 
-    plt.scatter(x, y, c='g', marker=("."))
-    # plt.set_title("Stations")
+    for i, txt in enumerate(n):
+        plt.annotate(txt, (y[i], x[i]))
 
-    # plt.tight_layout()
+    plt.title('Stations')
+
+    plt.tight_layout()
+    plt.xlim(-1, 15)
+    plt.ylim(-1, 15)
     plt.show()
     plt.savefig("filename.jpeg", format="jpeg")
 

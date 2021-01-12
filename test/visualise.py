d7bd3c9deb24957810c7c582fc106b5020f6c622
Jet Eliens <jetelie@live.nl>
2021-01-12 10:18:26 +0100

    updated

--- test/visualise.py
+++ test/visualise.py
@@ -1,12 +1,24 @@
-# import matplotlib
-# matplotlib.use('TkAgg')
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
 
 def visualise():
+
+    plt.figure(figsize=(2.61538461538, 3))
+    ax = plt.axes(projection=ccrs.EuroPP())
+    ax.add_feature(cartopy.feature.OCEAN)
+    ax.add_feature(cartopy.feature.COASTLINE)
+    ax.add_feature(cartopy.feature.BORDERS, linestyle='-', alpha=.5)
+    # ax.coastlines(resolution='50m')
+    ax.gridlines()
+
+    ax.set_extent([-150, 60, -25, 60])
+    # img_extent = (-120.67660000000001, -106.32104523100001, 13.2301484511245, 30.766899999999502)
+
     x = [3, 5, 9, 12]
     y = [2, 6, 8, 13]
     n = ['Amsterdam', 'Alkmaar', 'Haarlem', 'Delft']
+
 
     # BBox = ((df.longitude.min(3.2932),   df.longitude.max(6.2869),      
         #  df.latitude.min(52.9701), df.latitude.max(51.6998))
@@ -16,19 +28,18 @@ def visualise():
     #     x.append(station.x_position)
     #     y.append(station.y_position)
 
-    # img = plt.imread("kaart1.png")
-    plt.figure()
-    # plt.imshow(img)
-    plt.plot(x, y, c='g', marker=("."))
 
-    for i, txt in enumerate(n):
-        plt.annotate(txt, (y[i], x[i]))
+    # plt.figure()
+    # plt.plot(x, y, c='g', marker=("."))
 
-    plt.title('Stations')
+    # for i, txt in enumerate(n):
+    #     plt.annotate(txt, (y[i], x[i]))
 
-    plt.tight_layout()
-    plt.xlim(-1, 15)
-    plt.ylim(-1, 15)
+    # plt.title('Stations')
+
+    # plt.tight_layout()
+    # plt.xlim(-1, 15)
+    # plt.ylim(-1, 15)
     plt.show()
     plt.savefig("filename.jpeg", format="jpeg")
 

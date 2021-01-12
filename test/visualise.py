99c5f97c623ddaa412abb6bd2075d62af471b89a
Jet Eliens <jetelie@live.nl>
2021-01-12 15:56:45 +0100

    updated

--- test/visualise.py
+++ test/visualise.py
@@ -1,48 +1,44 @@
-import matplotlib.colors as mcolors
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
 
 def visualise():
+    x = [4.7, 4.7, 4.9, 4.9]
+    y = [52.1, 52.6, 52.3, 52.4]
+    n = ['Alphen a/d Rijn', 'Alkmaar', 'Amsterdam Amstel', 'Amsterdam Centraal']
 
-    plt.figure(figsize=(2.61538461538, 3))
-    ax = plt.axes(projection=ccrs.EuroPP())
-    ax.add_feature(cartopy.feature.OCEAN)
-    ax.add_feature(cartopy.feature.COASTLINE)
-    ax.add_feature(cartopy.feature.BORDERS, linestyle='-', alpha=.5)
-    # ax.coastlines(resolution='50m')
-    ax.gridlines()
+    # df = pd.read_csv(r"StationsHolland.csv")
+    map = plt.imread(r"nederland.png")
 
-    ax.set_extent([-150, 60, -25, 60])
-    # img_extent = (-120.67660000000001, -106.32104523100001, 13.2301484511245, 30.766899999999502)
+    # print(df.head())
 
-    x = [3, 5, 9, 12]
-    y = [2, 6, 8, 13]
-    n = ['Amsterdam', 'Alkmaar', 'Haarlem', 'Delft']
+    fig, ax = plt.subplots(figsize=(7,9))
 
+    # print(df.head(2))
 
-    # BBox = ((df.longitude.min(3.2932),   df.longitude.max(6.2869),      
-        #  df.latitude.min(52.9701), df.latitude.max(51.6998))
+    ax.set_title('Trajectories')
+
+    BBox = (3.395, 7.273, 53.593, 50.716)
+    ax.set_xlim(BBox[0], BBox[1])
+    ax.set_ylim(BBox[2], BBox[3])
+
+    ax.plot(x, y, c='b', marker=("."))
+
+    for i, txt in enumerate(n):
+        plt.annotate(txt, (y[i], x[i]))
+
+    ax.imshow(map, zorder=0, extent = BBox, aspect= 'equal')
+    plt.show()
+    plt.savefig("trajectories.jpeg", format="jpeg")
+
+if __name__ == '__main__':
+    
+    visualise()
 
 
     # for station in kaart.stations:
     #     x.append(station.x_position)
     #     y.append(station.y_position)
 
-
-    # plt.figure()
-    # plt.plot(x, y, c='g', marker=("."))
-
-    # for i, txt in enumerate(n):
-    #     plt.annotate(txt, (y[i], x[i]))
-
-    # plt.title('Stations')
-
     # plt.tight_layout()
-    # plt.xlim(-1, 15)
-    # plt.ylim(-1, 15)
-    plt.show()
-    plt.savefig("filename.jpeg", format="jpeg")
 
-if __name__ == '__main__':
-    
-    visualise()

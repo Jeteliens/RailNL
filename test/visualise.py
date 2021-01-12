46e507d67e93c219f9108ac6ad86c28776729a71
Jet Eliens <jetelie@live.nl>
2021-01-12 16:35:53 +0100

    updated

--- test/visualise.py
+++ test/visualise.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-​
+
 def visualise():
     # x = [4.7, 4.7, 4.9, 4.9]
     # y = [52.1, 52.6, 52.3, 52.4]
@@ -11,46 +11,30 @@ def visualise():
     map = plt.imread(r"nederland.png")
 
     print(df.head())
+    
+    x = []
+    y = []
+    n = []
 
     fig, ax = plt.subplots(figsize=(7,9))
 
     ax.set_title('Trajectories')
-​
+
     BBox = (3.395, 7.273, 53.593, 50.716)
     ax.set_xlim(BBox[0], BBox[1])
     ax.set_ylim(BBox[2], BBox[3])
-​
+
     ax.plot(x, y, c='b', marker=("."))
-​
+
     for i, txt in enumerate(n):
         plt.annotate(txt, (y[i], x[i]))
-​
+
     ax.imshow(map, zorder=0, extent = BBox, aspect= 'equal')
     plt.show()
     plt.savefig("trajectories.jpeg", format="jpeg")
-​
+
 if __name__ == '__main__':
     
     visualise()
-<<<<<<< HEAD
-​
-    # plt.tight_layout()
-​
-    # x = []
-    # y = []
-​
-    # for station in df:
-    #     print(station)
-    #     x.append(station[2])
-=======
 
     # plt.tight_layout()
-
-    # x = []
-    # y = []
-
-    # for station in df:
-    #     print(station)
-    #     x.append(station[2])
-    #     y.append(station[3])
->>>>>>> ec0f2d22634c97600b94c8617d30eecb3e1ad81d

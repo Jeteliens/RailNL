08324caa88bebab8c557943bee0ee8fe861ce583
MMElBaghdadi <elbaghdadi.mouad@gmail.com>
2021-01-15 17:03:59 +0100

    depth_first.py updated

--- code/algorithms/depth_first.py
+++ code/algorithms/depth_first.py
@@ -7,3 +7,25 @@ class DepthFirst:
         
         self.best_map
     
+    
+    def run(self):
+        """
+        Runs the algorithm untill all possible states are visited.
+        """
+        while self.stations:
+            new_graph = self.get_next_station()
+
+            # Retrieve the next empty node.
+            node = new_graph.get_empty_node()
+
+            if node is not None:
+                self.build_children(new_graph, node)
+            else:
+                # Stop if we find a solution
+                # break
+
+                # or ontinue looking for better graph
+                self.check_solution(new_graph)
+
+        # Update the input graph with the best result found.
+        self.graph = self.best_solution
--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -77,30 +77,3 @@ def randomise_train(map, number_of_trains, time_frame):
     # determine the number of ridden connections
     map.number_of_ridden_connections = len(ridden_connections)
     # print(map.number_of_ridden_connections)
-
-
-
-# def random_assignment(map, possibilities):
-#     for station in map.stations.values():
-#         station.set_value(random.choice(possibilities))
-
-# def random_reconfigure_station(map, station, possibilities):
-#     station.set_value(random.choice(possibilities))
-
-# def random_reconfigure_stations(map, stations, possibilities):
-#     for station in stations:
-#         random_reconfigure_station(map, station, possibilities)
-
-# def random_reassignment(map, possibilities):
-#     new_map = copy.deepcopy(map)
-
-#     random_assignment(new_map, possibilities)
-
-#     violating_stations = new_map.get_violations()
-
-#     while len(violating_stations):
-#         random_reconfigure_stations(new_map, violating_stations, possibilities)
-
-#         violating_stations = new_map.get_violations()
-
-#     return new_map

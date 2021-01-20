446d38a739a5aa35b93b55ca65760710dab6b1f2
MMElBaghdadi <elbaghdadi.mouad@gmail.com>
2021-01-20 11:57:33 +0100

    update

--- code/algorithms/hill_climber.py
+++ code/algorithms/hill_climber.py
@@ -9,7 +9,8 @@ class HillClimber:
 
     def __init__(self, map):
         self.map = map
-        self.original_map = map
+        self.old_map = map
+        self.best_map = map
 
 
         # trains = [{'train': 'train_1', 'stations': [Helmond, Eindhoven, Tilburg, Breda, Etten-Leur, Roosendaal, Vlissingen]}]
@@ -17,7 +18,11 @@ class HillClimber:
     def run(self, number_of_runs):
         old_score = self.map.score
         
+        # print(self.map.all_ridden_connections)
+
         for i in range(number_of_runs):
+            # print(self.map.all_ridden_connections)
+            
             for trajectory in self.map.trains:
                 randomise = Randomise(self.map)
                 new_train_data = randomise.create_train()
@@ -31,24 +36,45 @@ class HillClimber:
                 self.map.total_distance = self.map.total_distance + new_train_distance - old_train_distance
 
                 # remove the ridden connections in the old train from the list
-                for cnx_id in self.determine_ridden_connections(old_train):
+                ridden_connections_old_train = self.determine_ridden_connections(old_train)
+                # print(f"ridden_connections_old_train: {ridden_connections_old_train}")
+                # if i == 0:
+                #     print(f"Before removing: {self.map.all_ridden_connections}")
+                for cnx_id in ridden_connections_old_train:
+                    # if cnx_id in self.map.all_ridden_connections:
                     self.map.all_ridden_connections.remove(cnx_id)
+                # if i == 0:
+                #     print(f"After removing: {self.map.all_ridden_connections}")
                 
                 # add the ridden connections in the new train to the list
-                # print(self.determine_ridden_connections(new_train))
+                # print(f"New Train: {new_train}")
+                # print(f"{self.determine_ridden_connections(new_train)}")
+                # print(self.map.all_ridden_connections)
                 self.map.all_ridden_connections += self.determine_ridden_connections(new_train)
 
+                # if i == 0:
+                #     print(f"Before: {self.map.ridden_connections}")
                 self.map.ridden_connections = self.remove_duplicates(self.map.all_ridden_connections)
+                # if i == 0:
+                #     print(f"After: {self.map.ridden_connections}")
 
                 # print(self.map.all_ridden_connections)
                 self.map.number_of_ridden_connections = len(self.map.all_ridden_connections)
+                print(f"Number of ridden connections: {self.map.number_of_ridden_connections}")
 
                 new_score = self.map.calculate_score()
 
+                # if new_score > old_score:
+                #     self.best_map = self.map
+                
                 if new_score < old_score:
-                    self.map = self.original_map
+                    # self.best_map = self.old_map
+                    self.map = self.old_map
+                else:
+                    self.best_map = self.map
+                    self.old_map = self.map
 
-        return self.map
+        return self.best_map
 
     def determine_ridden_connections(self, train):
 
@@ -66,15 +92,16 @@ class HillClimber:
 
         # print(connections_list)
 
-        with open(self.map.connections_file, 'r') as in_file:
-            reader = csv.DictReader(in_file)
-
-            for connection in connections_list:
+        for connection in connections_list:
+            with open(self.map.connections_file, 'r') as in_file:
+                reader = csv.DictReader(in_file)
 
                 connection_id = 0
                 for row in reader:
                     connection_id += 1
-                    # print(f"row['station1']: {row['station1']}")
+                    # print(f"{connection_id}" , end="")
+                    # print(f"row['station1']: {row['station1']} / row['station2']: {row['station2']} || ", end="")
+                    # print(f"connection[0]: {connection[0]} / connection[1]: {connection[1]}")
                     # print(f"connection[0]: {connection[0]}")
                     if row['station1'] == connection[0].name and row['station2'] == connection[1].name:
                         # print("condition 1 is met")
@@ -84,9 +111,14 @@ class HillClimber:
                         # print("condition 2 is met")
                         ridden_connections.append(connection_id)
                         # break
+                    # else:
+                    #     print("No condition met")
+                
+                # print(f"Ridden connections: {ridden_connections}")
+                # print("---------------")
 
-        print(f"Ridden connections: {ridden_connections}")
-        print("================")
+        # print(f"Ridden connections: {ridden_connections}")
+        # print("===========================================")
         return ridden_connections
 
     def remove_duplicates(self, input_list):
